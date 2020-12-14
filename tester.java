3cac557670d3b33e7d403c7c5e84b398dee68b4d
emmachio <emmany2409@gmail.com>
2020-12-14 00:50:50 -0500

    done

--- tester.java
+++ tester.java
@@ -1,11 +1,23 @@
 public class tester{
 
   public static void main(String[] args) {
-    NoNullArrayList<String> test = new NoNullArrayList<String>();
+    OrderedArrayList<String> test = new OrderedArrayList<String>();
     test.add("0");
+    System.out.println(test);
     test.add("1");
-    test.add(null);
+    System.out.println(test);
+    test.add("3");
+    // test.add(2, null);
+    System.out.println(test);
     test.add("2");
+    System.out.println(test);
+    System.out.println(test.set(1,"7"));
+    System.out.println(test);
+    test.add("1");
+    System.out.println(test);
+    test.add("1");
+    System.out.println(test);
+    test.add("1");
     System.out.println(test);
   }
 }
